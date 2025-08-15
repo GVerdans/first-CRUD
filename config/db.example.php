70fb94a0fea09d543d3f4e5ff2b443053ffd0ce6
Gabriel Verdan dos Santos <tahoriemverdan@gmail.com>
2025-08-15 18:07:32 -0300

    pull README.md

---
+++ config/db.example.php
@@ -0,0 +1,17 @@
+<?php
+
+DEFINE('HOST', '#');
+DEFINE('PORT', '#');
+DEFINE('DBNAME', '#');
+DEFINE('USER', '#');
+DEFINE('PSWD', '#');
+
+try {
+    $pdo = new PDO("mysql: host=" . HOST . ";port=" . PORT . ";dbname=" . DBNAME . ";user=" . USER . ";password=" . PSWD);
+    // echo "Deu certo garai";
+
+} catch (PDOException $e){
+    die ("A conexão com o DB " . DBNAME . "falhou devido ao erro " . $e->getMessage());
+}
+
+?>
